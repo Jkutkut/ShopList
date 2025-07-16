a990f48aee2e7bd0c8d072b76144e5807d9444fb
jkutkut <45950243+Jkutkut@users.noreply.github.com>
2025-07-16 19:15:44 +0200

    refactor: response json with correct error codes

--- backend/api/src/route_error.rs
+++ backend/api/src/route_error.rs
@@ -1,35 +1,51 @@
-use rocket::{catch, Responder};
+use rocket::catch;
 use rocket::serde::json::Json;
+use rocket::response::Responder;
+use rocket::Request;
+use rocket::http::Status;
 
-#[catch(404)]
-pub fn not_found() -> Json<&'static str> {
-  Json("Resource not found.")
+#[derive(serde::Serialize)]
+pub struct InvalidResponse{
+	status: Status,
+	message: String,
 }
 
-#[catch(501)]
-pub fn not_implemented() -> Json<&'static str> {
-  Json("This feature is not implemented yet.")
+impl InvalidResponse {
+	fn new(status: Status, message: String) -> Self {
+		Self { status, message }
+	}
+}
+
+impl<'r> Responder<'r, 'static> for InvalidResponse {
+	fn respond_to(self, _req: &'r Request<'_>) -> rocket::response::Result<'static> {
+		rocket::response::status::Custom(self.status, Json(self)).respond_to(_req)
+	}
+}
+
+fn msg(status: Status, msg: &str) -> InvalidResponse {
+	InvalidResponse::new(status, msg.to_string())
+}
+
+#[catch(401)]
+pub fn unauthorized() -> InvalidResponse {
+	msg(Status::Unauthorized, "Unauthorized")
+}
+
+#[catch(404)]
+pub fn not_found() -> InvalidResponse {
+	msg(Status::NotFound, "Not Found")
+}
+
+pub fn invalid_api(reason: &str) -> InvalidResponse {
+	msg(Status::BadRequest, reason)
 }
 
 #[catch(500)]
-pub fn internal_server_error() -> Json<&'static str> {
-  Json("Something went wrong on our side.")
+pub fn internal_server_error() -> InvalidResponse {
+	msg(Status::InternalServerError, "Internal Server Error")
 }
 
-#[derive(Responder)]
-#[response(status = 409, content_type = "json")]
-pub struct InvalidAPI {
-  message: String
-}
-
-impl From<String> for InvalidAPI {
-	fn from(message: String) -> InvalidAPI {
-		InvalidAPI { message }
-	}
-}
-
-impl From<&str> for InvalidAPI {
-	fn from(message: &str) -> InvalidAPI {
-		message.to_string().into()
-	}
+#[catch(501)]
+pub fn not_implemented() -> InvalidResponse {
+	msg(Status::NotImplemented, "Not Implemented")
 }
