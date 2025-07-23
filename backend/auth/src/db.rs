3bed95bdfdf3b6d3842ddba716d5b1e0816465cb
jkutkut <45950243+Jkutkut@users.noreply.github.com>
2025-07-23 22:25:11 +0200

    feat: auth password hash

--- backend/auth/src/db.rs
+++ backend/auth/src/db.rs
@@ -1,17 +1,26 @@
 use tokio_postgres::Client;
 use uuid::Uuid;
+use argon2::{
+	password_hash::{
+		rand_core::OsRng,
+		PasswordHasher, SaltString,
+	},
+	Argon2
+};
 
 pub struct ShoplistDbAuth {
-	db_client: Client
+	db_client: Client,
 }
 
 impl ShoplistDbAuth {
 	pub fn new(db_client: Client) -> Self {
-		Self { db_client }
+		Self {
+			db_client,
+		}
 	}
 
 	pub async fn basic_login(&self, username: String, password: String) -> Result<String, ()> {
-		let password = self.encrypt_password(password);
+		println!("Searching for {} with password '{}'", &username, &password);
 		let user_id = self.login_basic_user(username, password).await?;
 		Ok("OK".to_string()) // TODO
 	}
@@ -19,7 +28,10 @@ impl ShoplistDbAuth {
 
 impl ShoplistDbAuth {
 	fn encrypt_password(&self, password: String) -> String {
-		password // TODO
+		let salt = SaltString::generate(&mut OsRng);
+		let argon2 = Argon2::default();
+		let hash = argon2.hash_password(password.as_bytes(), &salt).unwrap();
+		hash.to_string()
 	}
 
 	async fn login_basic_user(&self, username: String, password: String) -> Result<Uuid, ()> {
