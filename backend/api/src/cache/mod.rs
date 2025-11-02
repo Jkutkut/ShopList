dcfb71cc0fc6fceb6252577a35b95a82afca6164
jkutkut <45950243+Jkutkut@users.noreply.github.com>
2025-11-02 02:09:48 +0100

    feat: api JSON based cache with valkey

--- backend/api/src/cache/mod.rs
+++ backend/api/src/cache/mod.rs
@@ -1,9 +1,12 @@
-use fred::prelude::*;
 use std::time::Duration;
+use serde::{
+	Serialize,
+	de::DeserializeOwned,
+};
 use log::*;
 use crate::utils::env_var;
 
-pub use fred::prelude::Client;
+pub use fred::prelude::*;
 
 pub async fn init_or(url: &str) -> Result<Client, Error> {
 	let config = Config::from_url(&url)?;
@@ -34,4 +37,76 @@ pub async fn init() -> Result<Client, String> {
 		Ok(())
 	});
 	Ok(client)
+}
+
+/// Obtains the given value from the given function.
+///
+/// The value is cached with the given key and expiration.
+pub async fn cached_value<T, E, Ft, F>(
+	cache_client: &Client,
+	key: &str,
+	expiration: Option<Expiration>,
+	ft_fetch_value: Ft,
+) -> Result<T, E>
+where
+	T: Serialize + DeserializeOwned,
+	Ft: FnOnce() -> F,
+	F: std::future::Future<Output = Result<T, E>>,
+{
+	debug!("Obtaining obj for key {}", key);
+	match try_get_cached_value(cache_client, key).await {
+		Some(obj) => return Ok(obj),
+		_ => info!("Not found in cache"),
+	}
+	match ft_fetch_value().await {
+		Ok(obj) => {
+			info!("Obj obtained from fetch");
+			cache_value(cache_client, key, &obj, expiration).await;
+			Ok(obj)
+		},
+		Err(e) => Err(e),
+	}
+}
+
+// TODO use binary for cache
+
+async fn try_get_cached_value<T>(
+	cache_client: &Client,
+	key: &str,
+) -> Option<T>
+where
+	T: DeserializeOwned,
+{
+	match cache_client.get::<Option<String>, _>(key).await {
+		Ok(Some(obj)) => {
+			let obj: T = serde_json::from_str(&obj).unwrap();
+			debug!("Obj obtained from cache");
+			return Some(obj);
+		},
+		Err(e) => error!("Failed to get from cache: {}", e),
+		_ => (),
+	};
+	None
+}
+
+async fn cache_value<T>(
+	cache_client: &Client,
+	key: &str,
+	value: &T,
+	expiration: Option<Expiration>,
+) where
+	T: Serialize,
+{
+	info!("Caching {key}");
+	let json = serde_json::to_string(&value).unwrap(); // TODO why can this fail?
+	debug!("JSON: {}", json);
+	match cache_client.set::<String, _, _>(
+		key, json,
+		expiration,
+		None, // Options
+		false, // Get
+	).await {
+		Ok(_) => debug!("Obj set in cache"),
+		Err(e) => error!("Failed to set obj in cache: {}", e),
+	}
 }
--- backend/api/src/guards.rs
+++ backend/api/src/guards.rs
@@ -6,15 +6,21 @@ use rocket::{
 	},
 	http::Status,
 };
+use serde::{
+	Deserialize, Serialize,
+};
+use log::*;
+use crate::{
+	grpc,
+	cache,
+};
+use model::UuidWrapper;
 use model::grpc::auth::{
 	UserToken,
 	User as GrpcUser
 };
-use model::UuidWrapper;
-use crate::grpc;
-use log::*;
 
-#[derive(Debug, serde::Serialize, serde::Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct User {
 	uuid: UuidWrapper,
 	name: String,
@@ -44,24 +50,40 @@ impl<'r> FromRequest<'r> for User {
 	type Error = ();
 
 	async fn from_request(req: &'r Request<'_>) -> Outcome<Self, Self::Error> {
+		let invalid = || Outcome::Error((Status::Unauthorized, ()));
+		let expiration = Some(cache::Expiration::EX(15 * 60)); // TODO
+		info!("User guard");
 		let authorization = req.headers().get_one("Authorization");
-		debug!("User from request token {:#?}", &authorization);
 		let token = match authorization {
 			Some(token) => token.split_once("Bearer ").unwrap().1.to_string(),
-			_ => return Outcome::Error((Status::Unauthorized, ())),
+			_ => return invalid(),
 		};
-		let mut auth_grpc_client = grpc::connect_auth().await.unwrap();
-		let auth_request = tonic::Request::new(UserToken { token });
-		match auth_grpc_client.me(auth_request).await {
-			Ok(response) => {
-				let response = response.into_inner();
-				debug!("user: {:#?}", response);
-				match Self::try_from(response).map_err(|_| ()) {
-					Ok(user) => Outcome::Success(user),
-					Err(_) => Outcome::Error((Status::Unauthorized, ())),
+		debug!("Authentication token: {}", &token);
+		let cache_client = req.rocket().state::<cache::Client>().unwrap();
+		let cache_key = token.clone();
+		let try_get_user = || async {
+			info!("Attempt to get user from grpc");
+			let mut auth_grpc_client = grpc::connect_auth().await.unwrap();
+			let auth_request = tonic::Request::new(UserToken { token });
+			let user = match auth_grpc_client.me(auth_request).await {
+				Ok(response) => {
+					let response = response.into_inner();
+					debug!("user: {:#?}", response);
+					match Self::try_from(response) {
+						Ok(user) => user,
+						Err(_) => return Err(()),
+					}
 				}
-			}
-			Err(_) => Outcome::Error((Status::Unauthorized, ())),
+				Err(_) => return Err(()),
+			};
+			Ok(user)
+		};
+		match cache::cached_value(
+			&cache_client, &cache_key, expiration,
+			try_get_user
+		).await {
+			Ok(user) => Outcome::Success(user),
+			Err(_) => invalid(),
 		}
 	}
 }
