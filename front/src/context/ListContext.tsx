1af1b0260dd69aa97ab5c46e1c6c8942da9fa311
jkutkut <45950243+Jkutkut@users.noreply.github.com>
2026-01-19 22:33:49 +0100

    feat: front ready for persistence layer

--- front/src/context/ListContext.tsx
+++ front/src/context/ListContext.tsx
@@ -1,6 +1,7 @@
 import React, { createContext, useEffect, useReducer } from "react";
 import { ListActionType, listReducer, type ListAction } from "./listReducer";
 import { CATEGORIES, LIST_PRODUCTS, PRODUCTS } from "../mockup";
+import useCachedValue, { NO_EXPIRATION } from "../hooks/useCachedValue";
 
 interface ListContextType {
     id: string;
@@ -40,26 +41,72 @@ interface ListContextProviderProps {
 const ListContextProvider = ({ id, children }: ListContextProviderProps) => {
     const [state, dispatch] = useReducer(listReducer, createListContext(id));
 
+    // TODO replace mockup with real fetch functions
+    const {
+        value: categories,
+        isLoading: isLoadingCategories
+    } = useCachedValue<any[]>({
+        key: `list-${id}-categories`,
+        fetchFunc: async () => {
+            console.debug(`Fetching categories for list ${id} from mockup`);
+            return CATEGORIES.filter((c) => c.listId === id);
+        },
+        expiration: NO_EXPIRATION,
+    });
+    const {
+        value: listProducts,
+        isLoading: isLoadingListProducts
+    } = useCachedValue<any[]>({
+        key: `list-${id}-list-products`,
+        fetchFunc: async () => {
+            console.debug(`Fetching list products for list ${id} from mockup`);
+            return LIST_PRODUCTS[id as keyof typeof LIST_PRODUCTS] || [];
+        },
+        expiration: NO_EXPIRATION,
+    });
+    const {
+        value: products,
+        isLoading: isLoadingProducts
+    } = useCachedValue<any[]>({
+        key: `list-${id}-products`,
+        fetchFunc: async () => {
+            console.debug(`Fetching products for list ${id} from mockup`);
+            return PRODUCTS;
+        },
+        expiration: NO_EXPIRATION,
+    });
+
     useEffect(() => {
-        // TODO Simulate loading
-        setTimeout(() => {
+        if (!isLoadingCategories && categories) {
             dispatch({
                 type: ListActionType.SET_CATEGORIES,
-                payload: CATEGORIES.filter((c) => c.listId === id)
+                payload: categories
             });
-        }, 100);
-        setTimeout(() => {
+        }
+    }, [isLoadingCategories, categories]);
+    useEffect(() => {
+        if (!isLoadingListProducts && listProducts) {
             dispatch({
                 type: ListActionType.SET_LIST_PRODUCTS,
-                payload: LIST_PRODUCTS[id as keyof typeof LIST_PRODUCTS] || []
-            })
-        }, 100);
-        setTimeout(() => {
+                payload: listProducts
+            });
+        }
+    }, [isLoadingListProducts, listProducts]);
+    useEffect(() => {
+        if (!isLoadingProducts && products) {
             dispatch({
                 type: ListActionType.SET_PRODUCTS,
-                payload: PRODUCTS,
-            })
-        }, 100);
+                payload: products
+            });
+        }
+    }, [isLoadingProducts, products]);
+
+    // TODO handle live updates with server
+    useEffect(() => {
+        const bc = new BroadcastChannel(`list-${id}`);
+        bc.onmessage = (event) => dispatch(JSON.parse(event.data));
+
+        return () => bc.close();
     }, [id]);
 
     return <ListContext.Provider value={{...state, dispatch}}>
