61ca20a5e2c510c0a72c0088f745241c2b197c63
jkutkut <45950243+Jkutkut@users.noreply.github.com>
2026-01-24 23:04:56 +0100

    feat: front login form functional

--- front/src/components/form/LoginForm.tsx
+++ front/src/components/form/LoginForm.tsx
@@ -1,21 +1,82 @@
+import { useState, type FormEvent } from "react";
 import useForm from "../../hooks/useForm";
-import { TextField, FormValidationState, TextFieldType } from "./textField";
+import { FormValidationState, TextField, TextFieldType } from "./textField";
+import userService from "../../api/versions/v1/userService";
+import type { FetchTransportError } from "../../api/transport/fetchTransport";
+import type { TransportError } from "../../api/types";
+import ValidationFeedback from "./ValidationFeedback";
 
 const LoginForm = () => {
-    const { username, password, onChange } = useForm({
-        username: "",
+    const { email, password, onChange } = useForm({
+        email: "",
         password: ""
     });
+    const [ feedback, setFeedback ] = useState<string>("");
+
+    const emailValidator = (value: string) => {
+        if (!value) {
+            return FormValidationState.ERROR;
+        }
+        const regex = /^[a-z][a-z1-9._-]*@[a-z]+\.[a-z]{1,3}$/;
+        if (!regex.test(value)) {
+            return FormValidationState.ERROR;
+        }
+        return FormValidationState.SUCCESS;
+    };
+    const passwordValidator = (value: string) => {
+        if (!value) {
+            return FormValidationState.ERROR;
+        }
+        return FormValidationState.SUCCESS;
+    };
+    const isFormValid = [
+        emailValidator(email),
+        passwordValidator(password)
+    ].every((v) => v === FormValidationState.SUCCESS);
+
+    const onInputChange = (e: FormEvent) => {
+        if (feedback !== "") {
+            setFeedback("");
+        }
+        onChange(e);
+    }
+    const onSubmit = async (e: FormEvent) => {
+        e.preventDefault();
+        if (!isFormValid) {
+            return;
+        }
+        const r = await userService.loginBasic({ email, password });
+        if (r.isErr()) {
+            const error: TransportError<FetchTransportError> = r.unwrapErr();
+            console.error("Log in error.", error.detail.message, error);
+            onChange({ target: { name: "password", value: "" }});
+            if (error.status == 400) {
+                setFeedback(`Unable to log in: email or password is incorrect`);
+                return;
+            }
+            else {
+                setFeedback(`Not able to log in: ${error.detail.message}`);
+                console.error(error);
+            }
+            return;
+        }
+        const tokenResponse = r.unwrap().data;
+        localStorage.setItem("token", JSON.stringify(tokenResponse)); // TODO change
+        window.location.href = "/";
+    }
+
     return <section className="full-screen-form">
         <form className="col gap">
             <h1>Login</h1>
             <TextField
-                name="username"
-                label="Username or Email"
+                name="email"
+                label="Email"
                 type={TextFieldType.EMAIL}
-                autocomplete="username email nickname"
-                initialValue={username}
-                onChange={onChange}
+                autocomplete="email"
+                initialValue={email}
+                validate={emailValidator}
+                errorMessage="Enter a valid email"
+                onChange={onInputChange}
             />
             <TextField
                 name="password"
@@ -23,7 +84,9 @@ const LoginForm = () => {
                 type={TextFieldType.PASSWORD}
                 autocomplete="current-password"
                 initialValue={password}
-                onChange={onChange}
+                validate={passwordValidator}
+                errorMessage="Enter a password"
+                onChange={onInputChange}
             />
             <a
                 href="/register"
@@ -32,7 +95,18 @@ const LoginForm = () => {
             >
                 Don't have an account? Register instead
             </a>
-            <button className="btn btn-primary" type="submit">Login</button>
+            <button type="submit"
+                className="btn btn-primary"
+                onClick={onSubmit}
+                // TODO disable if not valid
+            >
+                Login
+            </button>
+            <ValidationFeedback
+                isOn={feedback !== ""}
+                type={"invalid"}
+                message={feedback}
+            />
         </form>
     </section>;
 };
